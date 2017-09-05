d82e00e4af13ecd0286357cb8ad72e88e3dd89b6
nriddiford <nickriddiford@me.com>
2017-09-05 18:04:23 +0200

    Add test script and data

--- script/test.pl
+++ script/test.pl
@@ -3,10 +3,13 @@ use warnings;
 use strict;
 
 use vcfParse;
+use FindBin;
 
-my $vcf_file = '/Users/Nick_curie/Desktop/script_test/mutationProfiles/data/A573R25_filtered.snv.vcf';
+my $vcf_file = $FindBin::Bin . "/" . '../data/test_in.vcf';
 
 my ( $data, $info_fields, $filtered_vars ) = vcfParse::parse($vcf_file);
+
+printf "%-10s %-10s %-s\n" , "Var", "Genotype", "Allele frequency";
 
 for ( sort { @{ $data->{$a}}[0] cmp @{ $data->{$b}}[0] or
 		 @{ $data->{$a}}[1] <=> @{ $data->{$b}}[1]
@@ -14,7 +17,5 @@ for ( sort { @{ $data->{$a}}[0] cmp @{ $data->{$b}}[0] or
 	 my ( $chrom, $pos, $id, $ref, $alt, $quality_score, $filt, $info_block, $format_block, $tumour_info_block, $normal_info_block, $filters, $samples ) = @{ $data->{$_} };
 	 my (%sample_info)  = @{ $info_fields->{$_}->[6] };
 
-	 if ($sample_info{$_}{'TUMOR'}{'AF'}){
-		 print "Allele frequency = $sample_info{$_}{'TUMOR'}{'AF'}\n";
-	 }
+	 printf "%-10s %-10s %-s\n", $_, $sample_info{$_}{'TUMOR'}{'GT'}, $sample_info{$_}{'TUMOR'}{'AF'};
  }
