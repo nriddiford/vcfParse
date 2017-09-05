b4b5ff71710222e8540d9facbc2d6837bf9e430e
nriddiford <nickriddiford@me.com>
2017-09-05 18:05:41 +0200

    Add test script and data

--- script/test.pl
+++ script/test.pl
@@ -9,7 +9,7 @@ my $vcf_file = $FindBin::Bin . "/" . '../data/test_in.vcf';
 
 my ( $data, $info_fields, $filtered_vars ) = vcfParse::parse($vcf_file);
 
-printf "%-10s %-10s %-s\n" , "Var", "Genotype", "Allele frequency";
+printf "%-10s %-10s %-10s %-s\n" , "Var", "Chrom", "Genotype", "Allele frequency";
 
 for ( sort { @{ $data->{$a}}[0] cmp @{ $data->{$b}}[0] or
 		 @{ $data->{$a}}[1] <=> @{ $data->{$b}}[1]
@@ -17,5 +17,5 @@ for ( sort { @{ $data->{$a}}[0] cmp @{ $data->{$b}}[0] or
 	 my ( $chrom, $pos, $id, $ref, $alt, $quality_score, $filt, $info_block, $format_block, $tumour_info_block, $normal_info_block, $filters, $samples ) = @{ $data->{$_} };
 	 my (%sample_info)  = @{ $info_fields->{$_}->[6] };
 
-	 printf "%-10s %-10s %-s\n", $_, $sample_info{$_}{'TUMOR'}{'GT'}, $sample_info{$_}{'TUMOR'}{'AF'};
+	 printf "%-10s %-10s %-10s %-s\n", $_, $chrom, $sample_info{$_}{'TUMOR'}{'GT'}, $sample_info{$_}{'TUMOR'}{'AF'};
  }
